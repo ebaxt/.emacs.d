2d5d9b64fb0f616f57522a7a4fbd4d8920d6b922
Erik Bakstad <erik.bakstad@gmail.com>
2011-11-03 18:46:31 +0100

    Added iy-go-to-char

--- init.el
+++ init.el
@@ -11,9 +11,20 @@
   (package-refresh-contents))
 
 ;; Add in your own as you wish:
-(defvar my-packages '(starter-kit starter-kit-eshell starter-kit-js starter-kit-lisp)
+(defvar my-packages '(starter-kit starter-kit-eshell starter-kit-js starter-kit-lisp starter-kit-bindings iy-go-to-char)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
+
+;; Completion that uses many different methods to find options.
+(global-set-key (kbd "C-.") 'hippie-expand)
+
+;; M-m: Move to char - similar to "f" in vim
+(global-set-key (kbd "M-m") 'iy-go-to-char)
+(global-set-key (kbd "M-M") 'iy-go-to-char-backward)
+
+;; Remap old M-m to M-i (better mnemonic for back-to-indentation)
+;; We lose tab-to-tab-stop, which is no big loss in my use cases.
+(global-set-key (kbd "M-i") 'back-to-indentation)
